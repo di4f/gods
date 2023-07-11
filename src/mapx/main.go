5c015810064ce2e91224bbc6548753db497b2a3d
surdeus <surdeus@gmail.com>
2023-07-11 20:01:04 +0300

    Fixed some Map issues.

--- src/mapx/main.go
+++ src/mapx/main.go
@@ -1,9 +1,22 @@
 package mapx
 
+import (
+	"fmt"
+)
+
 // The package implements some more specific
 // map structures for special uses.
 
-func Keys[K comparable, V any](m map[K] V) []K {
+// General map type, wrap for the built-in one.
+type Map[K comparable, V any] map[K] V
+
+// Returns new basic map.
+func New[K comparable, V any]() Map[K, V] {
+	return make(Map[K, V])
+}
+
+// Returns slice of keys of the map.
+func (m Map[K, V]) Keys() []K {
 	r := make([]K, 0, len(m))
 	for k := range m {
 		r = append(r, k)
@@ -12,7 +25,8 @@ func Keys[K comparable, V any](m map[K] V) []K {
 	return r
 }
 
-func Values[K comparable, V any](m map[K] V) []V {
+// Returns slice of values of the map.
+func (m Map[K, V]) Values(sm map[K] V) []V {
 	r := make([]V, 0, len(m))
 	for _, v := range m {
 		r = append(r, v)
@@ -21,12 +35,23 @@ func Values[K comparable, V any](m map[K] V) []V {
 	return r
 }
 
-func Reverse[K, V comparable](m map[K] V) map[V] K {
-	r := make(map[V] K)
-	for k, v := range m {
-		r[v] = k
-	}
+// Checks if the map contains the key.
+func (m Map[K, V]) Has(k K) bool {
+	_, ok := m[k]
+	return ok
+}
 
-	return r
+// Sets the new value by key or resets if it exists.
+func (m Map[K, V]) Set(k K, v V) {
+	m[k] = v
+}
+
+// Returns the value by key. Panics if there is no such key.
+func (m Map[K, V]) Get(k K) V {
+	v, ok := m[k]
+	if !ok {
+		panic(fmt.Sprintf("there is no such key '%v'", k))
+	}
+	return v
 }
 
